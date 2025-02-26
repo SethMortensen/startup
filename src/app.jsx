9bf635576ff58d5237676fdb90e69b89e37e2017
SethMortensen <sethrmortensen@gmail.com>
2025-02-26 12:37:37 -0700

    started react

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+    return (
+        <div className='app'>
+            <header>
+                <h1>GigaSnake</h1>
+                <h2>By Seth Mortensen</h2>
+            </header>
+
+            <main>
+                App components go here
+            </main>
+
+            <footer>
+                <a href="https://github.com/SethMortensen/startup">Repository</a>
+            </footer>
+        </div>
+    )
+}
