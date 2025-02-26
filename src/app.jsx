e60bcf923e3b986346f5e6005ed03d1dc4533eee
SethMortensen <sethrmortensen@gmail.com>
2025-02-26 13:02:04 -0700

    added navlinks for react

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login'
+import { Menu } from './menu/menu'
+import { Play } from './play/play'
+import { Leaderboards } from './leaderboards/leaderboards'
+
 export default function App() {
     return (
-        <div className='app'>
-            <header>
-                <h1>GigaSnake</h1>
-                <h2>By Seth Mortensen</h2>
-            </header>
+        <BrowserRouter>
+            <div className='app'>
+                <header>
+                    <h1>GigaSnake</h1>
+                    <h2>By Seth Mortensen</h2>
+                </header>
 
-            <main>
-                App components go here
-            </main>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='menu' element={<Menu />} />
+                    <Route path='play' element={<Play />} />
+                    <Route path='Leaderboards' element={<Leaderboards />} />
+                    <Route path='*' element={<Notfound />} />
+                </Routes>
 
-            <footer>
-                <a href="https://github.com/SethMortensen/startup">Repository</a>
-            </footer>
-        </div>
+                <footer>
+                    <a href="https://github.com/SethMortensen/startup">Repository</a>
+                </footer>
+            </div>
+        </BrowserRouter>
     )
+}
+
+function Notfound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>
 }
